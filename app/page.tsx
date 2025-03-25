a78e492a07a6082a3c4e33066b534899e78b0c3a
Kelvin Zhang <kz61847@uga.edu>
2025-03-25 18:16:40 -0400

    Created. Hero background grid done

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import Hero from "@/components/Hero";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="bg-black">
+      <div>
+        
+        <Hero />
+      </div>
+    </main>
+  );
+}
