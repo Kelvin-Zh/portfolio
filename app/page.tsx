9e045cb65b6da1083ae8dd0db98d4192e059877f
Kelvin Zhang <kz61847@uga.edu>
2025-03-25 19:02:54 -0400

    Basic floating nav implemented

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from "@/components/Hero";
+import { FloatingNav } from "@/components/ui/floating-navbar";
 import Image from "next/image";
+import { FaHome } from "react-icons/fa";
 
 export default function Home() {
   return (
     <main className="bg-black">
       <div>
-        
+        <FloatingNav navItems={[ {name: 'Home', link: '/', icon: <FaHome />} ]}/>
         <Hero />
       </div>
     </main>
