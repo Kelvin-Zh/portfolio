e12a0a285fa7434180aeecec1799498a24d0ed5a
Kelvin Zhang <kz61847@uga.edu>
2025-03-27 14:45:18 -0400

    Removed grid from hero background

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -14,26 +14,7 @@ const Hero = () => {
             <Spotlight className="top-28 left-80 h-[80vh] w-[50vw]" fill="purple" />
         </div>
         
-        { /* Backgrounds/Grid */ }
-        <div className="flex h-screen w-full items-center justify-center bg-white bg-grid-white/[0.003] dark:bg-black dark:bg-grid-white/[0.003] absolute top-0 left-0">
-        <div
-        className={cn(
-          "absolute inset-0",
-          "[background-size:40px_40px]",
-          "[background-image:linear-gradient(to_right,#121212_1px,transparent_1px),linear-gradient(to_bottom,#e4e4e7_1px,transparent_1px)]",
-          "dark:[background-image:linear-gradient(to_right,#121212_1px,transparent_1px),linear-gradient(to_bottom,#121212_1px,transparent_1px)]",
-        )}
-        />
-
-        
-        {/* Radial gradient for the container to give a faded look */}
-        <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black"></div>
-            <p className="relative z-20 bg-gradient-to-b from-neutral-200 to-neutral-500 bg-clip-text py-8 text-4xl font-bold text-transparent sm:text-7xl">
-            
-            </p>
-        </div>
-
-        { /* Text in the grid (introduction) */ }
+        { /* Introduction */ }
         <div className="flex justify-center relative my-20 z-10">
             <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
                 <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  theme: {
-    extend: {
-      colors: {
-        black: {
-          DEFAULT: '#000',
-          100: '#000319'
-        }
-      }
-    }
-  }
+  
 };
 
 export default nextConfig;
