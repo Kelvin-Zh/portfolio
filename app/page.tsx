ae8e82fe373e7d9603ea35609b56df618873d292
Kelvin Zhang <kz61847@uga.edu>
2025-03-27 14:59:03 -0400

    meteor effect added

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
 import { FloatingNav } from "@/components/ui/floating-navbar";
+import { Meteors } from "@/components/ui/meteors";
 import Image from "next/image";
 import { FaHome } from "react-icons/fa";
 
 export default function Home() {
   return (
-    <main className="bg-black">
+    <main className="bg-black overflow-hidden w-full">
+      <Meteors /> 
       <div>
         <FloatingNav navItems={[ {name: 'Home', link: '/', icon: <FaHome />} ]}/>
         <Hero />
